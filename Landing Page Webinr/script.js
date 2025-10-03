57fd27f0ee3a30d99ad75ff63ee07eaa3109cc4d
Deepak <114180847+deepakbhalekar@users.noreply.github.com>
2025-10-04 00:06:45 +0530

    Add files via upload

---
+++ Landing Page Webinr/script.js
@@ -0,0 +1,27 @@
+// Set the date we're counting down to
+const countDownDate = new Date("Aug 15, 2025 14:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown-timer"
+    document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown-timer").innerHTML = "THIS WEBINAR HAS ENDED";
+    }
+}, 1000);
